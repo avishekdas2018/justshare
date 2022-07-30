062b00cc1404b41436c5d63f333479151805ba62
Avishek Das <dasavishek2013@gmail.com>
2022-07-30 11:49:12 +0530

    add cors

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const path = require('path');
+const cors = require('cors');
 const app = express();
 
 const PORT = process.env.PORT || 3000;
@@ -9,6 +10,13 @@ app.use(express.static('public'));
 
 const connectDB = require('./config/db');
 connectDB();
+
+// Cors settings
+const corsOption = {
+    origin: process.env.ALLOWED_CLIENT
+}
+
+app.use(cors(corsOption));
 
 // Templete Engine
 app.set('views', path.join(__dirname, '/views'));
